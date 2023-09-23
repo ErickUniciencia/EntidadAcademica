6c58fa5cd77571cd61d1e9af33931697b9110baa
Erick Martinez <ed.martinezgomez@unicienciabga.edu.co>
2023-09-23 07:40:09 -0500

    segunda interfaz

---
+++ app/src/main/java/com/example/entidadacademica/RegistroEstudiante.java
@@ -0,0 +1,13 @@
+package com.example.entidadacademica;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegistroEstudiante extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registro_estudiante);
+    }
+}
